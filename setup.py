4d69cfc69b698e6071a8e6fe8ae07a4b74136103
Colin Mason <masonc789@gmail.com>
2022-09-22 16:46:33 -0400

    revert and rename

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="codex.sh",
-    version="0.1",
+    name="codex-sh",
+    version="0.1.0",
     py_modules=["main"],
     install_requires=[
         "rich",
         "pyperclip",
         "openai",
-        "python-dotenv",
         "argparse",
     ],
-    data_files=[("prompts.txt", ["prompts.txt"])],
     entry_points={"console_scripts": ["codex=main:main"]}
 )
